b37ba4ab2cacf4276affcb6e21c62d1f516c9bfb
Elle Jacobs <enj657@gmail.com>
2021-11-22 19:02:34 -0600

    adding to Challenge file

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,3 +1,4 @@
+# Deliverable 1
 # Use the library function to load the dplyr package
 library(dplyr)
 
@@ -9,3 +10,14 @@ lm(mpg~.,data=mecha_car_df)
 
 # generate summary statistics for lm
 summary(lm(mpg~.,data=mecha_car_df))
+
+
+# Deliverable 2
+# import and read csv as a table
+suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F, stringsAsFactors = F)
+
+# create dataframe to get mean, median, variance, and sd of PSI
+total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+
+# create dataframe grouping by manufacturing_lot
+lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
